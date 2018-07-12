fa1761b4e11239193245102a37840216f77915d1
Eva Pey <40271421+EvaForEva@users.noreply.github.com>
2018-07-12 15:38:41 +0200

    Delete textConcatination.js

--- 00.Simple-calculations/textConcatination.js
+++
@@ -1,8 +0,0 @@
-function personalInfo(arguments) {
-    let firstName = arguments[0];
-    let lastName = arguments[1];
-    let age = Number(arguments[2]);
-    let city = arguments[3];
-    console.log("You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + city + ".");
-}
-personalInfo([Petar, Petrov, 24, Sofia])
