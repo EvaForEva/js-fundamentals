b1f80131d42dd9564c6494c529c3283b37574554
Eva Pey <40271421+EvaForEva@users.noreply.github.com>
2018-07-12 15:39:23 +0200

    Delete letters.js

--- 03.For-cicle/letters.js
+++
@@ -1,6 +0,0 @@
-function asciiCharacters() {
-    for(let currentCharacter = 97; currentCharacter <= 122; currentCharacter++) {
-        console.log(String.fromCharCode(currentCharacter));
-    }
-}
-asciiCharacters()
