80c6ea3be2427edddfdce06450854fbc1beedafe
Eva P <40271421+EvaForEva@users.noreply.github.com>
2019-09-16 11:39:39 +0200

    Update sequence2KTo1.js
    cleaning unneeded comments

--- 02.While-cycle/sequence2KTo1.js
+++ 02.While-cycle/sequence2KTo1.js
@@ -1,9 +1,9 @@
 function sequence(input) {
     let n = input[0];
-    let k = 1; //definirano tuk gore, zastoto taka e po uslovie. izpechatva se predi while cikula
+    let k = 1; 
     while(k <= n) {
         console.log(k);
-        k = (2 * k) + 1;// i sled kato e izpechatano tuk se modificira
+        k = (2 * k) + 1;
     }
 }
 sequence([3])
